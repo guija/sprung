bfe8e6383ec0958fc9706499c788b986e4978422
Guillermo Janner Acero <guillermo.janner@fau.de>
2016-02-29 23:33:55 +0100

    Translated tray icon menu. Added functionality to edit settings file to tray icon

--- Sprung/SystemTray.cs
+++ Sprung/SystemTray.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Sprung.Properties;
+using System.Diagnostics;
 
 namespace Sprung
 {
@@ -26,27 +27,38 @@ namespace Sprung
         private void InitializeSystemTray()
         {
             ToolStripMenuItem exit = new ToolStripMenuItem();
-            ToolStripMenuItem options = new ToolStripMenuItem();
+            ToolStripMenuItem settings = new ToolStripMenuItem();
             ToolStripMenuItem help = new ToolStripMenuItem();
             ToolStripMenuItem reloadSettings = new ToolStripMenuItem("Reload Settings");
 
             symbol.Icon = Resources.spring;
             symbol.Visible = true;
 
-            help.Text = "Hilfe";
-            options.Text = "Einstellungen";
-            exit.Text = "Beenden";
+            help.Text = "Help";
+            settings.Text = "Settings";
+            exit.Text = "Exit";
 
             cms.Items.Add(help);
-            cms.Items.Add(options);
+            cms.Items.Add(settings);
             cms.Items.Add(reloadSettings);
             cms.Items.Add(exit);
-            
 
+            help.Click += HelpCallback;
             exit.Click += new EventHandler(this.exit);
             reloadSettings.Click += new EventHandler(this.reloadSettings);
+            settings.Click += SettingsCallback;
 
             symbol.ContextMenuStrip = cms;
+        }
+
+        private void SettingsCallback(object sender, EventArgs e)
+        {
+            Process.Start("settings.json");
+        }
+
+        private void HelpCallback(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/guija/sprung/");
         }
 
         private void exit(object sender, EventArgs e)
