06f0ec2df8286a65cd0864ab40fab36721b9e883
Guillermo Janner Acero <guillermo@janneracero.es>
2018-09-20 19:50:04 +0200

    Added method monitoring for finding slow methods

---
+++ Sprung/MethodTimerLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprung
+{
+    public static class MethodTimeLogger
+    {
+        public static void Log(MethodBase methodBase, long milliseconds, string message)
+        {
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                w.WriteLine($"{methodBase.Name} {milliseconds}");
+            }
+        }
+    }
+}
